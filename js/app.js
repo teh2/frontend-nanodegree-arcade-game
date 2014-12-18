8b0c6e3c1c6fdb65590f4fab70520ac46faf47a1
tom <thillii@yahoo.com>
2014-12-18 15:45:09 -0600

    Organize constants in a "board" object. add enemies and get them moving.

--- js/app.js
+++ js/app.js
@@ -9,8 +9,11 @@ var board = {
 	"colWidth": 101,
 	"rowHeight": 83,
 	"playerStartRow": 5,
-	"playerStartCol": 2
-	
+	"playerStartCol": 2,
+	"playerYOffset": -10, //fudge factor to push player to middle of block
+	"enemyRowMin": 1,
+	"enemyRowMax": 3,
+	"enemyYOffset": -21 //Fudge factor to push enemy to middle of lane (in pixels)
 }
 
 // Enemies our player must avoid
@@ -21,7 +24,16 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+	this.init();
 }
+
+Enemy.prototype.init = function() {
+	this.x = 0 - board.colWidth;
+	this.y = this.startRow() * board.rowHeight + board.enemyYOffset;
+	this.speed = this.setSpeed();
+	this.delay = this.setDelay();
+}
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -29,11 +41,44 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+	if (0 < this.delay) {
+		this.delay -= dt;
+	} else {
+		this.x = this.x + this.speed * dt;
+		if ((board.cols * board.colWidth) < this.x) {
+			//Hey! We made it all the way across the board... let's start again...
+			this.init();
+		}
+	}
 }
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
+console.log("renderEnemy("+this.x+","+this.y+")");
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+//Helper that calculates the enemy's speed
+// speed is denoted in pixels per second
+Enemy.prototype.setSpeed = function() {
+	//ToDo: make this more "interesting"
+	return board.colWidth; //For now, move a constant column's width per second
+}
+
+//Helper that sets the delay before this enemy repeats its' trek across the screen
+// delay is in seconds.
+//For now, it's simply a random number from 1 to 5
+Enemy.prototype.setDelay = function() {
+	return Math.floor(Math.random() * 5) + 1;
+}
+
+// Helper that returns a random starting row within the bounds set by board.enemyRowMin and board.enemyRowMax constants.
+// IE: put the enemy in the starting gate at the edge of his lane.
+Enemy.prototype.startRow = function() {
+	var enemyRows = board.enemyRowMax - board.enemyRowMin + 1;
+	var enemyRow = Math.floor((Math.random() * enemyRows) + board.enemyRowMin);
+console.log("enemyRow:"+enemyRow);
+	return enemyRow;
 }
 
 // Now write your own player class
@@ -51,7 +96,7 @@ Player.prototype.update = function() {
 
 Player.prototype.render = function() {
 	//ToDo
-	ctx.drawImage(Resources.get(this.sprite), this.col * board.colWidth, this.row * board.rowHeight);
+	ctx.drawImage(Resources.get(this.sprite), this.col * board.colWidth, this.row * board.rowHeight + board.playerYOffset);
 }
 
 Player.prototype.stillOnBoard = function(col, row) {
@@ -85,6 +130,9 @@ Player.prototype.handleInput = function(key) {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
+for (var enemyIndex = 0; enemyIndex < 4; enemyIndex++) {
+	allEnemies.push(new Enemy());
+}
 var player = new Player();
 
 
--- js/engine.js
+++ js/engine.js
@@ -25,8 +25,10 @@ var Engine = (function(global) {
         ctx = canvas.getContext('2d'),
         lastTime;
 
-    canvas.width = 505;
-    canvas.height = 606;
+    // canvas.width = 505;
+    // canvas.height = 606;
+    canvas.width = board.canvas.width;
+    canvas.height = board.canvas.height;
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
@@ -115,8 +117,10 @@ var Engine = (function(global) {
                 'images/grass-block.png',   // Row 1 of 2 of grass
                 'images/grass-block.png'    // Row 2 of 2 of grass
             ],
-            numRows = 6,
-            numCols = 5,
+            // numRows = 6,
+            // numCols = 5,
+            numRows = board.rows,
+            numCols = board.cols,
             row, col;
 
         /* Loop through the number of rows and columns we've defined above
@@ -132,7 +136,8 @@ var Engine = (function(global) {
                  * so that we get the benefits of caching these images, since
                  * we're using them over and over.
                  */
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+                // ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+                ctx.drawImage(Resources.get(rowImages[row]), col * board.colWidth, row * board.rowHeight);
             }
         }
 
